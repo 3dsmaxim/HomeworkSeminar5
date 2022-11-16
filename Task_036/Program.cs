b2a190fb8643cbd525e5ff4b7a496d28cace3cdb
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-11-17 02:34:44 +0300

    Исправил 36 задачю

--- Task_036/Program.cs
+++ Task_036/Program.cs
@@ -8,20 +8,19 @@ int[] NewArray(int num)
     int[] arr = new int[num];
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(1, 100);
+        arr[i] = new Random().Next(-10, 10);
     }
     return arr;
 }
 
 void SumIndexNum(int[] arrayNum)
 {
-    int sum = 1;
+    int sum = 0;
     for (int i = 0; i < arrayNum.Length; i++)
     {
-        if (i % 2 != 0) sum *= arrayNum[i];
+        if (i % 2 != 0) sum += arrayNum[i];
 
     }
-    if (arrayNum.Length == 1) sum = 0;
     Console.Write($" сумма элементов, стоящих на нечётных позициях равен {sum}");
 }
 
